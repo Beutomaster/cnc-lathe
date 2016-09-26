9b14a7bf24010cbf5fa382c9501df0cb5d5a830f
Beutomaster <Beutomaster@users.noreply.github.com>
2016-09-26 14:37:27 +0200

    PIN-Tests
    PIN-Tests

--- Arduino MEGA Code/Arduino_PIN_Test/Arduino_PIN_Test.ino
+++ Arduino MEGA Code/Arduino_PIN_Test/Arduino_PIN_Test.ino
@@ -1,21 +1,21 @@
 
-#define MAX_PIN 65
+#define MAX_PIN 69
 
 int i=0;
 
 void setup() {                
   // initialize all pins as output
-  for (i=0; i<MAX_PIN; i++) {
+  for (i=0; i<=MAX_PIN; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
 void loop() {
-  for (i=0; i<MAX_PIN; i++) {
+  for (i=0; i<=MAX_PIN; i++) {
     digitalWrite(i, HIGH);   // set all LEDs on
   }
     delay(1000);              // wait for a second
-  for (i=0; i<MAX_PIN; i++) {
+  for (i=0; i<=MAX_PIN; i++) {
     digitalWrite(i, LOW);    // set all LEDs off
   }
     delay(1000);              // wait for a second
