73a1099f93bffcb054eb07cd76ad2763cbc7a31e
Beutomaster <Beutomaster@users.noreply.github.com>
2017-01-01 21:59:45 +0100

    Webinterface pipe_server with select timeout

--- Raspberry Pi 3/SPI-Communication/pipe_server.c
+++ Raspberry Pi 3/SPI-Communication/pipe_server.c
@@ -1,7 +1,10 @@
 /* pipe_server.c */
+// compile with: gcc -o pipe_server pipe_server.c
 //test with second console: echo "test" >> /home/pi/spi_com/arduino_pipe.tx
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/select.h>
+//#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -16,75 +19,123 @@
 char state=0;
 int exclusive = 0;
 
-static void send_to_arduino (const char *text, int client_sid) {
-   //output message
-   printf ("Message from Client-SESSION: %i\n", client_sid);
-   printf ("Message %s\n", text);
-   return;
+//static void send_to_arduino (const char *text, const char *client_sid) {
+void send_to_arduino (const char *text) {
+	//output message
+	//printf ("Message from Client-SESSION: %i\n", client_sid);
+	printf ("Message from Client-SESSION: %s\n", text);
 }
 
 int main (void) {
-   char buffer[BUF], answer_to_client[BUF], answer_fifo_name[BUF], client_sid[27];
-   int r_fd, w_fd, n, i;
-   
-   //allow all rights for new created files
-   umask(0);
-   
-   //Server creates arduino_pipe.tx, if it does not exist
-   if (mkfifo ("arduino_pipe.tx", O_RDWR | 0666) < 0) {
-      //arduino_pipe.tx exists
-      if(errno == EEXIST)
-         printf ("arduino_pipe.tx exists, trying to use it!\n");
-      else {
-         perror("mkfifio()");
-         exit (EXIT_FAILURE);
-      }
-   }
-   
-   //Server opens arduino_pipe.tx readonly
-   r_fd = open ("arduino_pipe.tx", O_RDONLY);
-   if (r_fd == -1) {
-      perror ("open(1)");
-      exit (EXIT_FAILURE);
-   }
-   
-   while (1) { //Server-Loop
-      if (read (r_fd, buffer, BUF) != 0) {
-		/*
-		//get SESSION-ID of calling client
-		n = 0, i = 0;
-		while (buffer[n] != '\n')
-		client_sid[i++] = buffer[n++];
-		pid[++i] = '\n';
+	char buffer[BUF], answer_to_client[BUF], answer_fifo_name[BUF], client_sid[27];
+	int r_fd, w_fd, i;
 
-		//set answer_fifo_name
-		strcpy (answer_fifo_name, "client_session_pipe.");
-		strncat (answer_fifo_name, client_sid, i);
+	//allow all rights for new created files
+	umask(0);
 
-		//set answer to client
-		answer_to_client[0] = '\0';
-		if ( exclusive != atoi(client_sid)) strcpy (answer_to_client, MSG_ERROR_CLIENT);
-		else {
-			if (!state) strcpy (answer_to_client, MSG_SUCCESS);
-			else strcpy (answer_to_client, MSG_ERROR_STATE);
+	//Server creates arduino_pipe.tx, if it does not exist
+	if (mkfifo ("arduino_pipe.tx", O_RDWR | 0666) < 0) {
+	  //arduino_pipe.tx exists
+	  if(errno == EEXIST)
+		 printf ("arduino_pipe.tx exists, trying to use it!\n");
+	  else {
+		 perror("mkfifio()");
+		 exit (EXIT_FAILURE);
+	  }
+	}
+
+	//Server opens arduino_pipe.tx readonly
+	/*r_fd = open ("arduino_pipe.tx", O_RDONLY);
+	You opened that FIFO as read only (O_RDONLY), whenever there is no writer to the FIFO, the read end will receive an EOF.
+	Select system call will return on EOF and for every EOF you handle there will be a new EOF. This is the reason for the observed behavior.
+	To avoid this open that FIFO for both reading and writing (O_RDWR). This ensures that you have at least one writer on the FIFO thus there wont be an EOF and as a result select won't return unless someone writes to that FIFO.
+	*/
+	//Server opens arduino_pipe.tx
+	r_fd = open ("arduino_pipe.tx", O_RDWR);
+	if (r_fd == -1) {
+	  perror ("open(1)");
+	  exit (EXIT_FAILURE);
+	}
+	
+	//set parameter for select
+	fd_set r_fd_set;
+	struct timeval timeout;
+	int ret;
+
+	while (1) { //Server-Loop
+		/* Initialize the file descriptor r_fd_set. */
+		// has to be done every iteration, but why?
+		FD_ZERO(&r_fd_set);
+		FD_SET(r_fd, &r_fd_set);
+	
+		/* Initialize the timeout data structure. */
+		// has to be done every iteration, because select may update the timeout argument to indicate how much time was left
+		timeout.tv_sec = 1; //seconds
+		timeout.tv_usec = 0; //microseconds
+	
+		/* In the interest of brevity, I'm using the constant FD_SETSIZE, but a more
+		   efficient implementation would use the highest fd + 1 instead. In this case
+		   with a single fd, you can replace FD_SETSIZE with
+		   r_fd+1 thereby limiting the number of fds the system has to
+		   iterate over. */
+		//ret = select(FD_SETSIZE, &r_fd_set, NULL, NULL, &timeout);
+		ret = select(r_fd+1, &r_fd_set, NULL, NULL, &timeout);
+
+		if (ret == 0) //timeout
+		{
+			//printf("select timeout after 1s waiting for message on pipe!\n");
+			printf("Updating Machine-State after waiting for message on pipe for 1s!\n");
 		}
-						
-		if (strlen(answer_to_client)) {
-			//open client_session_pipe send answer
-			w_fd = open (answer_fifo_name, O_WRONLY);
-			if (w_fd == -1) {
-			perror ("open(2)");
-			exit (EXIT_FAILURE);
+		else if (ret < 0) //error
+		{
+			printf("select returned with an error while waiting for message on pipe!\n");
+		}
+		else //there was activity on the file descripor
+		{
+			//printf("select says pipe is readable\n");
+			
+			if (read (r_fd, buffer, BUF) != 0) {
+				/*
+				//get SESSION-ID of calling client
+				i = 0;
+				while (buffer[i] != '\n') {
+					client_sid[i] = buffer[i];
+					i++;
+				}
+				
+				//get SESSION-ID of calling client
+				pid = ;
+				*/
+
+				/*
+				//set answer_fifo_name
+				strcpy (answer_fifo_name, "client_session_pipe.");
+				strncat (answer_fifo_name, client_sid, i);
+
+				//set answer to client
+				answer_to_client[0] = '\0';
+				if ( exclusive != atoi(client_sid)) strcpy (answer_to_client, MSG_ERROR_CLIENT);
+				else {
+					if (!state) strcpy (answer_to_client, MSG_SUCCESS);
+					else strcpy (answer_to_client, MSG_ERROR_STATE);
+				}
+								
+				if (strlen(answer_to_client)) {
+					//open client_session_pipe send answer
+					w_fd = open (answer_fifo_name, O_WRONLY);
+					if (w_fd == -1) {
+						perror ("open(2)");
+						exit (EXIT_FAILURE);
+					}
+					write (w_fd, answer_to_client, strlen(answer_to_client));
+					close (w_fd);
+				}
+				*/
+
+				//process message
+				send_to_arduino (buffer);
 			}
-			write (w_fd, answer_to_client, strlen(answer_to_client));
-			close (w_fd);
 		}
-		*/
-
-		//process message
-		send_to_arduino (buffer);
-      }
-      sleep(1);
-   }
-   return EXIT_SUCCESS;
+	}
+	return EXIT_SUCCESS;
 }
